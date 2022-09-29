bf54f0761fda45c42b5a4eae57b20bd5756f5791
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-09-29 15:11:37 -0700

    fixed some formatting

--- part-1/date_difference.cc
+++ part-1/date_difference.cc
@@ -13,13 +13,7 @@
 #include <iostream>
 int main(int argc, char const *argv[]) {
   std::cout << "Let's find the number of days between two dates...\n";
-  // TODO: Using cout, prompt the user for a starting month
   std::cout << "Enter a start month: ";
-
-
-
-  // TODO: declare a variable named start_month of type int, assign the value 0
-  // to the variable.
   int start_month{0};
   std::cin >> start_month;
   std::cout << "Enter a start day: ";
@@ -60,49 +54,5 @@ int end_number_of_days_since_epoch{0};
   std::cout << "The number of days between " << start_month << "/" << start_day <<
   "/" << start_year << " and " << end_month << "/" << end_day <<
   "/" << end_year <<" is " << number_days << " days" << "\n";
-
-
-
-  //std::cout << "Debug: " << end_number_of_days_s/ starting number of days:
-
-  // TODO: Calculate the Julian day of the starting date using the formula given
-  // in the README.md and assign the result to the variable
-  // start_number_of_days_since_epoch. Remember you will need to use the
-  // variables start_day, start_month, and start_year. The formula is very
-  // complicated and has many parantheses. Be very careful and double check your
-  // work.
-
-  // Optional TODO: To help you figure out what you are doing, print out the
-  // value of start_number_of_days_since_epoch. For example: cout << "Debug:
-  // starting number of days: " << start_number_of_days_since_epoch << "\n";
-  // This way you can check your work. When you believe everything is working,
-  // comment out this line or remove it. This is how programmers check their
-  // work as they write a program.
-
-  // TODO: Calculate the Julian day of the ending date using the formula given
-  // in the README.md and assign the result to the variable
-  // end_number_of_days_since_epoch. Remember you will need to use the variables
-  // end_day, end_month, and end_year. The formula is very complicated and has
-  // many parantheses. Be very careful and double check your work.
-
-  // Optional TODO: To help you figure out what you are doing, print out the
-  // value of end_number_of_days_since_epoch. For example:
-  //   cout << "Debug: ending number of days: " <<
-  //   end_number_of_days_since_epoch << "\n";
-
-  // This way you can check your work. When you believe everything is working,
-  // comment out this line or remove it. This is how programmers check their
-  // work as they write a program.
-
-  // TODO: Declare a variable named number_days of type int, assign the value 0
-  // to the variable.
-  // TODO: Calculate the number of days between end_number_of_days_since_epoch
-  // and start_number_of_days_since_epoch by using subtraction. Assign the
-  // result to number_days.
-
-  // TODO: summarize the input by telling the computer user the two dates in
-  // question and then how many days are between the two dates. See the
-  // README.md for examples.
-
   return 0;
 }
