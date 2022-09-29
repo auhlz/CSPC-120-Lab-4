ea8f226c585549927e4bd7fb37b2de9772f20139
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-09-29 16:47:27 -0700

    fixed formatting working on lint now

--- part-1/date_difference.cc
+++ part-1/date_difference.cc
@@ -24,35 +24,38 @@ int main(int argc, char const *argv[]) {
   std::cin >> start_year;
   std::cout << "\n";
 
-  std::cout << "Enter a end month: ";
+  std::cout << "Enter an end month: ";
   int end_month{0};
   std::cin >> end_month;
-  std::cout << "Enter a end day: ";
+  std::cout << "Enter an end day: ";
   int end_day{0};
   std::cin >> end_day;
-  std::cout << "Enter a end year: ";
+  std::cout << "Enter an end year: ";
   int end_year{0};
   std::cin >> end_year;
   std::cout << "\n";
 
   int start_number_of_days_since_epoch{0};
-  int start_julian_day = start_day - 32075 + 1461
-      * (start_year + 4800 + (start_month - 14) / 12) / 4
-      + 367 * (start_month - 2 - (start_month - 14) / 12 * 12) / 12 - 3
-      * ((start_year + 4900 + (start_month - 14) / 12) / 100) / 4;
+  int start_julian_day =
+      start_day - 32075 +
+      1461 * (start_year + 4800 + (start_month - 14) / 12) / 4 +
+      367 * (start_month - 2 - (start_month - 14) / 12 * 12) / 12 -
+      3 * ((start_year + 4900 + (start_month - 14) / 12) / 100) / 4;
   start_number_of_days_since_epoch = start_julian_day;
 
-int end_number_of_days_since_epoch{0};
-  int end_julian_day = end_day - 32075 + 1461
-      * (end_year + 4800 + (end_month - 14) / 12) / 4
-      + 367 * (end_month - 2 - (end_month - 14) / 12 * 12) / 12 - 3
-      * ((end_year + 4900 + (end_month - 14) / 12) / 100) / 4;
+  int end_number_of_days_since_epoch{0};
+  int end_julian_day =
+      end_day - 32075 + 1461 * (end_year + 4800 + (end_month - 14) / 12) / 4 +
+      367 * (end_month - 2 - (end_month - 14) / 12 * 12) / 12 -
+      3 * ((end_year + 4900 + (end_month - 14) / 12) / 100) / 4;
   end_number_of_days_since_epoch = end_julian_day;
 
   int number_days{0};
-  number_days = end_number_of_days_since_epoch - start_number_of_days_since_epoch;
-  std::cout << "The number of days between " << start_month << "/" << start_day <<
-  "/" << start_year << " and " << end_month << "/" << end_day <<
-  "/" << end_year <<" is " << number_days << " days" << "\n";
+  number_days =
+      end_number_of_days_since_epoch - start_number_of_days_since_epoch;
+  std::cout << "The number of days between " << start_month << "/" << start_day
+            << "/" << start_year << " and " << end_month << "/" << end_day
+            << "/" << end_year << " is " << number_days << " days"
+            << "\n";
   return 0;
 }
