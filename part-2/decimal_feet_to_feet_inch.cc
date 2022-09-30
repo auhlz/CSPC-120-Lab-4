554255c1065b8c2985451767c72f427528370fa6
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-09-29 22:07:11 -0700

    fixed format

--- part-2/decimal_feet_to_feet_inch.cc
+++ part-2/decimal_feet_to_feet_inch.cc
@@ -1,107 +1,58 @@
-// TODO: Add the required header
-
+// Alex Labitigan
+// CPSC 120-12
+// 2022-09-29
+// alexlabitigan@csu.fullerton.edu
+// @auhlz
+//
+// Lab 04-02
+// Partners: @farrah-e
+//
 // Program to convert decimalized feet to feet, inches, and fractional inches,
 // up to one eigth.
 
 #include <cmath>
 #include <iostream>
-
 using namespace std;
 
-// TODO: Define a constant of type float with the name kInchesInFeet
-// which is assigned the value 12.0
+const float kInchesInFeet{12.0};
+const float kEighthsInInch{8.0};
 
-// TODO: Define a constant of type float with the name kEighthsInInch
-// which is assigned the value 8.0
-
-// Main function - the entry point to our program, it does not take command line
-// arguments
 int main(int argc, char const *argv[]) {
+  float input_decimal_feet{NAN};
+  cout << "Enter the number of feet you wish to convert to feet-inch: ";
+  cin >> input_decimal_feet;
+  string sign;
+  if (input_decimal_feet < 0.0) {
+    input_decimal_feet *= -1.0;
+    sign = "-";
+  }
 
-  // TODO: Declare a variable of type float with the name input_decimal_feet and
-  // assign NAN to it.
+  int _feet_integer_component{0};
+  float feet_fractional_component{NAN};
 
-  // TODO: Prompt the computer user to enter the number of feet to convert. See
-  // the README.md for examples.
+  _feet_integer_component = static_cast<int>(trunc(input_decimal_feet));
+  feet_fractional_component =
+      input_decimal_feet - float(_feet_integer_component);
+  float decimal_inches{NAN};
+  decimal_inches = feet_fractional_component * kInchesInFeet;
 
-  // TODO: Read from the terminal using cin to set input_decimal_feet to the
-  // input
+  int _inches_integer_component{0};
+  float inches_fractional_component{NAN};
+  _inches_integer_component = static_cast<int>(decimal_inches);
+  inches_fractional_component =
+      decimal_inches - float(_inches_integer_component);
 
-  // TODO: Declare a variable named sign of type string. You do
-  // not need to initialize strings because they are set to an empty string by
-  // default. An empty string is "".
+  float eighths{NAN};
+  eighths = inches_fractional_component * kEighthsInInch;
+  int _eighths_integer_component{0};
+  _eighths_integer_component = static_cast<int>(eighths);
 
-  // TODO: Using input_decimal_feet, write an if statement. If
-  // input_decimal_feet is less than 0.0, then multiply input_decimal_feet by
-  // -1.0 and assign the result back to input_decimal_feet. Assign the variable
-  // sign the string "-". (Else, do nothing - you do not need an else or else if
-  // statement.)
-
-  // TODO: Declare a variable of type int with the name _feet_integer_component
-  // and assign 0 to it.
-
-  // TODO: Declare a variable of type float with the name
-  // feet_fractional_component and assign NAN to it.
-
-  // TODO: Using the explanation in the README.md, truncate input_decimal_feet
-  // and assign the result to _feet_integer_component. For example:
-  //  _feet_integer_component = static_cast<int>(input_decimal_feet);
-
-  // TODO: Using the explanation in the README.md, take the difference between
-  // input_decimal_feet and _feet_integer_component and assign it to
-  // feet_fractional_component
-  // For example:
-  // feet_fractional_component = input_decimal_feet -
-  // float(_feet_integer_component);
-
-  // TODO Optional: You may want to print out your values at this point to
-  // verify that things are working as you expect. Remember to remove any extra
-  // print statements when you have your program working to your satisfaction.
-
-  // TODO: Declare a variable of type float with the name decimal_inches and
-  // assign NAN to it.
-
-  // TODO: Multiply feet_fractional_component by kInchesInFeet and assign it to
-  // decimal_inches.
-
-  // TODO: Declare a variable of type int with the name
-  // _inches_integer_component and assign 0 to it.
-
-  // TODO: Declare a variable of type float with the name
-  // inches_fractional_component and assign NAN to it.
-
-  // TODO: Using the explanation in the README.md, truncate decimal_inches and
-  // assign the result to _inches_integer_component.
-
-  // TODO: Using the explanation in the README.md, take the difference between
-  // decimal_inches and _inches_integer_component and assign it to
-  // inches_fractional_component
-
-  // TODO Optional: You may want to print out your values again at this point to
-  // verify that things are working as you expect. Remember to remove any extra
-  // print statements when you have your program working to your satisfaction.
-
-  // TODO: Declare a variable of type float with the name eighths and
-  // assign NAN to it.
-  // TODO: Multiply inches_fractional_component by kEightsInInch and assign it
-  // to eighths.
-
-  // TODO: Declare a variable of type int with the name
-  // _eighths_integer_component and assign 0 to it.
-
-  // TODO: Using the explanation in the README.md, truncate eighths and
-  // assign the result to _eighths_integer_component.
-
-  // TODO: Using cout, print to the the terminal the sign and
-  // input_decimal_feet.
-
-  // TODO: Using cout, print the sign and the _feet_integer_component.
-
-  // TODO: Write and if statement. If eighths_integer_component != 0, then,
-  // using cout, print out eighths_integer_component along with the string "/8"
-  // to make it look like a fraction.
-
-  // TODO: Using cout, print "inches\n" to terminate the string.
-
+  cout << sign << input_decimal_feet << " feet is ";
+  cout << sign << _feet_integer_component << " feet, ";
+  cout << _inches_integer_component;
+  if (_eighths_integer_component != 0) {
+    cout << " and " << _eighths_integer_component << "/8";
+  }
+  cout << " inches\n";
   return 0;
 }
